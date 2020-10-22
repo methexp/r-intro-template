cdd53e51a0120c3a59f070bc1c1a3876d2b8c6bc
mariusbarth <marius.barth@uni-koeln.de>
2020-10-22 17:06:29 +0200

    Initialize template

---
+++ example.R
@@ -0,0 +1,9 @@
+
+
+# Beispiel-Code
+x <- 1 + 1:4
+
+
+
+# Klicke den Button 'Source' am oberen rechten Rand dieses Fensters, um den
+# gesamten R-Code, der sich in dieser Datei befindet, auszuführen.
